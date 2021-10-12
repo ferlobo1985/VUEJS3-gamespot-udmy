aaf7a21f954969ed05c6b008efe5aeb75c16c39a
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-10-12 17:27:30 -0300

    07-Creating the sign in form

--- src/routes.js
+++ src/routes.js
@@ -4,13 +4,15 @@ import { createRouter, createWebHistory, START_LOCATION } from 'vue-router';
 // COMPONENTS
 import Home from './components/Home';
 import Article from './components/Articles/article.vue';
+import Signin from './components/User/signin.vue';
 
 
 const routes = createRouter({
     history: createWebHistory(),
     routes:[
         { path:'/', component: Home, name:'home'},
-        { path:'/article/:id', component:Article, name:'article' }
+        { path:'/article/:id', component:Article, name:'article' },
+        { path:'/signin', component:Signin, name:'signin' }
     ]
 })
 
