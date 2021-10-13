cc2ad7cb73a7a247af71c7894db54aa9361e16e4
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-10-13 16:32:43 -0300

    20- Add article form elements - part two

--- src/components/User/Dashboard/Admin/add.vue
+++ src/components/User/Dashboard/Admin/add.vue
@@ -18,8 +18,82 @@
             </Field>
          </div>
          
+         <br/>
 
+         <div class="form-group">
+            <Field name="title" v-slot="{ field, errors, errorMessage }">
+               <form-element
+                  :field="field"
+                  :errors="errors"
+                  :errorMessage="errorMessage"
+                  element="input"
+                  type="text"
+                  placeholder="Title of the article"
+               />
+            </Field>
+         </div>
 
+  
+         <br/>
+
+         <div class="form-group">
+            <Field name="excerpt" v-slot="{ field, errors, errorMessage }">
+               <form-element
+                  :field="field"
+                  :errors="errors"
+                  :errorMessage="errorMessage"
+                  element="textarea"
+                  placeholder="Add an excerpt"
+                  :rows="3"
+               />
+            </Field>
+         </div>
+
+         <br/>
+
+         <div class="form-group">
+            <Field 
+               name="rating" 
+               v-slot="{ field, errors, errorMessage }"
+               value="Select a rating"
+            >
+               <form-element
+                  :field="field"
+                  :errors="errors"
+                  :errorMessage="errorMessage"
+                  element="select"
+               >
+                  <option value="Select a rating" selected>Select a rating</option>
+                  <option v-for="rating in ratings" :key="rating" :value="rating">
+                     {{ rating }}
+                  </option>
+               </form-element>
+            </Field>
+         </div>
+         
+         <br/>
+
+         <div class="form-group">
+            <Field name="img" v-slot="{ field, errors, errorMessage }">
+               <form-element
+                  :field="field"
+                  :errors="errors"
+                  :errorMessage="errorMessage"
+                  element="input"
+                  type="text"
+                  placeholder="Add the source of the img"
+               />
+            </Field>
+         </div>
+
+         <br/>
+
+         <button
+         type="submit"
+         class="btn btn-primary"
+         >
+            Add article
+         </button>
    </Form>
 
 </template>
@@ -41,6 +115,7 @@ export default {
    },
    data(){
       return {
+         ratings:[1,2,3,4,5],
          formSchema:addArticleSchema
       }
    },
