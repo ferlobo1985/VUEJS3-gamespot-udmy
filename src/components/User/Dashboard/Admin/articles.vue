2b4b97227377605d1b66a069e9f6602f6dab16d5
ferlobo1985 <fernando.lobo.prez@gmail.com>
2021-10-14 00:04:41 -0300

    24- Get Admin Articles

--- src/components/User/Dashboard/Admin/articles.vue
+++ src/components/User/Dashboard/Admin/articles.vue
@@ -7,10 +7,24 @@
 
 <script>
 import DashboardTitle from '../../../Utils/dashTitle.vue';
+import { mapActions } from 'vuex'
 
 export default {
    components:{
       DashboardTitle
+   },
+   computed:{
+      adminArticles(){
+         return this.$store.getters['articles/getAdminArticles']
+      }
+   },
+   mounted(){
+      this.getAdminArticles({limit:3})
+   },
+   methods:{
+      ...mapActions('articles',[
+         'getAdminArticles'
+      ])
    }
 }
 </script>
